0e712f47f5f96a1fd851bee592f113ce60f4536c
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-07-16 20:46:21 +0545

    repository: ProductRepository

--- E-Commerce/src/main/java/com/springproject/ecommerce/repository/ProductRepository.java
+++ E-Commerce/src/main/java/com/springproject/ecommerce/repository/ProductRepository.java
@@ -1,9 +1,12 @@
 package com.springproject.ecommerce.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.springproject.ecommerce.model.Product;
 
-public interface ProductRepository extends JpaRepository<Product, Long>{
+public interface ProductRepository extends JpaRepository<Product, Integer>{
 
+	List<Product> findProductByCategoryId(Integer categoryId);
 }
