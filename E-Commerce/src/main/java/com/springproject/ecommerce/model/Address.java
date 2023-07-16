9e52071f7461b2fc5f8d07a328ae9ef12c721955
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-07-16 20:45:18 +0545

    model: Address

--- E-Commerce/src/main/java/com/springproject/ecommerce/model/Address.java
+++ E-Commerce/src/main/java/com/springproject/ecommerce/model/Address.java
@@ -15,24 +15,34 @@ import lombok.Data;
 @Table(name = "address")
 public class Address {
 
-	 	@Id
-	  @GeneratedValue(strategy = GenerationType.IDENTITY)
-	  @Column(name = "id", nullable = false)
-	  private Long id;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
+	private int id;
 
-	  @Column(name = "address_line_1", nullable = false, length = 512)
-	  private String addressLine1;
+	@Column(name = "address_line_1", nullable = false, length = 512)
+	private String addressLine1;
 
-	  @Column(name = "address_line_2", length = 512)
-	  private String addressLine2;
+    @Column(name = "address_line_2", length = 512)
+	private String addressLine2;
 
-	  @Column(name = "city", nullable = false)
-	  private String city;
+	@Column(name = "city", nullable = false)
+	private String city;
 	  
-	  @Column(name = "country", nullable = false, length = 75)
-	  private String country;
+	@Column(name = "state", nullable = false, length = 75)
+	private String state;
+	
+	@Column(name = "country", nullable = false, length = 75)
+	private String country;
+	
+	@Column(name = "shipping")
+    private boolean shipping;
+
+    @Column(name = "billing")
+    private boolean billing;
 	  
-	  @ManyToOne(optional = false)
-	  @JoinColumn(name = "user_id", nullable = false)
-	  private User user;
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
 }
