aee0be8627f363efe922aacb62ae3fd254191cd1
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-07-19 20:39:45 +0545

    controller: Cart

--- E-Commerce/src/main/java/com/springproject/ecommerce/controller/CartController.java
+++ E-Commerce/src/main/java/com/springproject/ecommerce/controller/CartController.java
@@ -1,16 +1,33 @@
 package com.springproject.ecommerce.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.springproject.ecommerce.model.Cart;
+import com.springproject.ecommerce.model.Product;
+import com.springproject.ecommerce.service.CartService;
+import com.springproject.ecommerce.service.ProductService;
 
 @Controller
 @RequestMapping("/cart")
 public class CartController {
 	
+	@Autowired
+	public ProductService prodService;
+	
+	@Autowired
+	public CartService cartservice;
+	
 	@GetMapping("/home")
 	public String getCart() {
 		return "shopping-cart";
 	}
+
 
 }
