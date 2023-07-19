9b441269a49c35e94ad27be17bd9634bf96cf4ae
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-07-19 20:39:53 +0545

    controller: Home

--- E-Commerce/src/main/java/com/springproject/ecommerce/controller/HomeController.java
+++ E-Commerce/src/main/java/com/springproject/ecommerce/controller/HomeController.java
@@ -1,15 +1,24 @@
 package com.springproject.ecommerce.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.springproject.ecommerce.service.ProductService;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
 	
+	@Autowired
+	public ProductService prodService;
+	
 	@GetMapping("/index")
-	public String home() {
+	public String home(Model model) {
+		
+		model.addAttribute("prodlist",prodService.findAllProduct());
 		return "Home";
 	}
 }
