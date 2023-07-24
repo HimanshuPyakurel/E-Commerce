d8b5bafcdcc1eb477d5602d99d0f561e640dc0fc
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-07-24 21:34:02 +0545

    model: Cart

--- E-Commerce/src/main/java/com/springproject/ecommerce/model/Cart.java
+++ E-Commerce/src/main/java/com/springproject/ecommerce/model/Cart.java
@@ -7,7 +7,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name = "cart")
 public class Cart {
