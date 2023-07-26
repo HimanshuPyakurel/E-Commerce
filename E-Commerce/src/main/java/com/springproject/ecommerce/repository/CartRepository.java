9d854de5b1a29bec03724c0044b4b4f72d34d083
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-07-26 22:09:37 +0545

    repository: CartRepository

--- E-Commerce/src/main/java/com/springproject/ecommerce/repository/CartRepository.java
+++ E-Commerce/src/main/java/com/springproject/ecommerce/repository/CartRepository.java
@@ -1,9 +1,14 @@
 package com.springproject.ecommerce.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.springproject.ecommerce.model.Cart;
+import com.springproject.ecommerce.model.Product;
 
 public interface CartRepository extends JpaRepository<Cart, Integer>{
 
+	public List<Cart> findByProduct(Product product);
+	
 }
