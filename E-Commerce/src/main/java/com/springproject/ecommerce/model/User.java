f4350a73ca9c075d986d36b444e0081b4be29744
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-07-26 22:08:40 +0545

    model: User

--- E-Commerce/src/main/java/com/springproject/ecommerce/model/User.java
+++ E-Commerce/src/main/java/com/springproject/ecommerce/model/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -25,4 +26,7 @@ public class User {
 	
 	private String email;
 	private String password;
+	
+	 @OneToOne(mappedBy = "user")
+	 private ShoppingCart shoppingCart;
 }
