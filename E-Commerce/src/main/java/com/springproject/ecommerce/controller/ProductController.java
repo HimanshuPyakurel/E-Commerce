50d8ffe48437af3c92fa1f8814297ab6b843ad1c
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-07-28 21:36:01 +0545

    controller: ProductController

--- E-Commerce/src/main/java/com/springproject/ecommerce/controller/ProductController.java
+++ E-Commerce/src/main/java/com/springproject/ecommerce/controller/ProductController.java
@@ -1,10 +1,13 @@
 package com.springproject.ecommerce.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -99,11 +102,24 @@ public class ProductController {
 	 public String view(@RequestParam int id, Model model) {
 		 
 		 model.addAttribute("prodObject",prodService.findProductById(id));
-		 model.addAttribute("catObject", catService.getCategoryById(id));
+		 model.addAttribute("catObject", catService.getAllCategory());
 		 
 		 return "productView";
 	 }
 	
+	    @GetMapping("/find")
+	    public String findProductById(@RequestParam int id, Model model){
+	    	
+	        Product product = prodService.findProductById(id);
+	        Long categoryId = product.getCategory().getId();
+	        List<Product> products = prodService.findProductByCategoryId(categoryId);
+	        model.addAttribute("product", product);
+	        model.addAttribute("products", products);
+	        
+	        return "redirect:/cart/add";
+	    }
+	 
+	 
 }	
 	
 	
