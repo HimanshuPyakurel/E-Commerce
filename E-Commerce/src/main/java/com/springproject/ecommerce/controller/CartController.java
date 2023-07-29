1839945c290135fdfdcf6e9a28b17efe3c28b1ca
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-07-29 20:25:50 +0545

    CartController

--- E-Commerce/src/main/java/com/springproject/ecommerce/controller/CartController.java
+++ E-Commerce/src/main/java/com/springproject/ecommerce/controller/CartController.java
@@ -1,5 +1,6 @@
 package com.springproject.ecommerce.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,13 +11,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.springproject.ecommerce.model.Address;
 import com.springproject.ecommerce.model.Cart;
-import com.springproject.ecommerce.model.Product;
 import com.springproject.ecommerce.model.User;
-import com.springproject.ecommerce.service.ShoppingCartService;
-import com.springproject.ecommerce.service.CategoryService;
+import com.springproject.ecommerce.service.AddressService;
+import com.springproject.ecommerce.service.CartService;
 import com.springproject.ecommerce.service.IUserService;
 import com.springproject.ecommerce.service.ProductService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/cart")
@@ -29,33 +33,116 @@ public class CartController {
 	public IUserService userservice;
 	
 	@Autowired
-	public ShoppingCartService cartservice;
+	public CartService cartservice;
 	
 	@Autowired
-	public CategoryService categoryservice;
+	public AddressService addressservice;
 	
 	@GetMapping("/show")
-	public String getHome(Model model,@ModelAttribute Product product,@RequestParam int id) {
+	public String show(HttpSession session, Model model) {
 		
-		model.addAttribute("prodlist",prodService.findProductById(id));
-		model.addAttribute("catlist",categoryservice.getAllCategory());
-		
-		
-		
-		Cart cart = new Cart();
-		double total = cart.getQuantity() * product.getProd_price();;
-		
-		return "Cart";
+    	model.addAttribute("total",total(session));
+		return "Cart"; 
 	}
 	
-	@PostMapping("/show")
-	public String postHome(Model model,@RequestParam int id, @ModelAttribute Cart cart) {
-		Product product = new Product();
-		model.addAttribute("prodlist",prodService.findProductById(id));
-		model.addAttribute("catlist",categoryservice.getAllCategory());
-		
-		
-		return "Cart";
-	}
+    @GetMapping("/add")
+    public String addCart(@RequestParam int id,HttpSession session, Model model)
+    {   
+    	if(session.getAttribute("cart")==null) {
+    		
+        	List<Cart> cart = new ArrayList<>();    	
+        	cart.add(new Cart(1,prodService.findProductById(id)));        	
+        	session.setAttribute("cart",cart);
+    		
+    	}else {
+    		List<Cart> cart = (List<Cart>) session.getAttribute("cart");
+    		int index = isExists(id, cart);
+    		
+    		if(index == -1) {
+    			cart.add(new Cart(1,prodService.findProductById(id)));
+    		}else {
+    			int quantity = cart.get(index).getQuantity() +1;
+    			cart.get(index).setQuantity(quantity);
+    		}
+    		session.setAttribute("cart",cart);
+    	} 
+    	
+    	model.addAttribute("total",total(session));
+             
+        return "Cart";
+    }
 	
+    @GetMapping("/delete")
+    public String deletecart(@RequestParam int id,HttpSession session,Model model)
+    {
+    	List<Cart> cart = (List<Cart>) session.getAttribute("cart");
+		int index = isExists(id, cart);
+    	cart.remove(index);
+    	session.setAttribute("cart",cart);	
+    	model.addAttribute("total",total(session));
+		return "Cart";
+    
+    }
+    
+    @PostMapping("/update")
+    public String updatecart(HttpServletRequest request,HttpSession session,Model model)
+    {
+    	String[] quantity = request.getParameterValues("quantity");
+    	List<Cart> cart = (List<Cart>) session.getAttribute("cart");
+    	
+    	for(int i=0;i<cart.size();i++) {
+    		cart.get(i).setQuantity(Integer.parseInt(quantity[i]));
+    	}
+    	session.setAttribute("cart",cart);
+    	model.addAttribute("total",total(session));
+		return "Cart";
+    
+    }
+    
+    @GetMapping("/checkout")
+    public String Checkoutcart(HttpSession session,@ModelAttribute User user,Model model)
+    {
+		User  usr = userservice.login(user.getUname(), user.getPassword());
+    	if(usr != null) {
+    		return "login";
+    	}else { 		
+    		List<Cart> cart = (List<Cart>) session.getAttribute("cart");
+    		cartservice.addItemToCart(cart);
+    		model.addAttribute("total",total(session));
+    		return "checkout";
+    	}	
+    
+    }
+    
+    @PostMapping("/checkout")
+    public String Checkoutcart(@ModelAttribute Address address){
+    	
+    	addressservice.addAddress(address);
+		return "cart";
+    }
+      
+    private int isExists(int id, List<Cart> cart) {
+    	for(int i=0; i<cart.size();i++) {
+    		if(cart.get(i).getProduct().getId() == id) {
+    			return i;   			
+    		}
+    	}
+    	return -1;   	
+    }
+    
+    private double total(HttpSession session) {    	
+    	List<Cart> cart = (List<Cart>) session.getAttribute("cart");
+    	double s = 0;
+    	for(Cart item: cart) {
+    		
+    		s+= item.getQuantity() * item.getProduct().getProd_price().doubleValue();
+    		
+    	}	
+    	return s;
+    }
+    
+       
+    
+    
+    
 }
