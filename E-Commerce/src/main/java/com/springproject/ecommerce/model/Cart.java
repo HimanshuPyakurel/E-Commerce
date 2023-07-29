1c592ed111cd33f7c7bcf7b1236f50f530c6ae08
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-07-29 20:22:25 +0545

    model: Cart

--- E-Commerce/src/main/java/com/springproject/ecommerce/model/Cart.java
+++ E-Commerce/src/main/java/com/springproject/ecommerce/model/Cart.java
@@ -1,14 +1,11 @@
 package com.springproject.ecommerce.model;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -18,22 +15,31 @@ import lombok.Data;
 @Table(name = "cart_items")
 public class Cart {
 	  @Id
-	  @GeneratedValue(strategy = GenerationType.IDENTITY)
+	  @GeneratedValue(strategy=GenerationType.IDENTITY) 
 	  private int id;
 
 	  @Column(name = "quantity")
 	  private int quantity;
 	  
 	  @Column(name = "total_price")
-	  private double totalPrice;
-	  
-	  @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-	  @JoinColumn(name = "shoppingcartId")
-	  private ShoppingCart cart;
+	  private double totalPrice;	  
 
-	   @OneToOne(fetch = FetchType.EAGER)
-	   @JoinColumn(name = "productId")
+	   @OneToOne(fetch = FetchType.LAZY)
 	   private Product product;
+
+	   
+	public Cart(int quantity, Product product) {
+		super();
+		this.quantity = quantity;
+		this.product = product;
+	}
+
+	public Cart() {
+		super();
+	}
+	
+	
+	
 	  
 	  
 }
