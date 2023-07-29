d31a5dcce3052b2de7b6da593a36b666127f515e
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-07-29 20:25:02 +0545

    CartServiceImpl

--- E-Commerce/src/main/java/com/springproject/ecommerce/service/impl/CartServiceImpl.java
+++ E-Commerce/src/main/java/com/springproject/ecommerce/service/impl/CartServiceImpl.java
@@ -1,9 +1,10 @@
 package com.springproject.ecommerce.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.springproject.ecommerce.model.Cart;
-import com.springproject.ecommerce.model.ShoppingCart;
 import com.springproject.ecommerce.repository.CartRepository;
 import com.springproject.ecommerce.service.CartService;
 
@@ -11,25 +12,27 @@ import com.springproject.ecommerce.service.CartService;
 public class CartServiceImpl implements CartService {
 
 	@Autowired
-	public CartRepository cartRepo;
+	private CartRepository cartRepo;
+
+	@Override
+	public void addItemToCart(List<Cart> cart) {
+
+		cartRepo.saveAll(cart);	
+	}
+
+	@Override
+	public void updateItemInCart(List<Cart> cart) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteItemFromCart(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
 	
-	@Override
-	public ShoppingCart addItemToCart(Cart cart) {
-		
-		return null;
-	}
-
-	@Override
-	public ShoppingCart updateItemInCart(Cart cart) {
-		
-		return null;
-	}
-
-	@Override
-	public ShoppingCart deleteItemFromCart(int id) {
-		
-		return null;
-	}
 
 
 	
