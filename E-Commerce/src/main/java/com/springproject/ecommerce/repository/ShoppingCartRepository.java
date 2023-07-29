bc17151af9bf8fc50b3cff5f730feab10ae0f7fa
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-07-29 20:23:44 +0545

    Deleted

--- E-Commerce/src/main/java/com/springproject/ecommerce/repository/ShoppingCartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springproject.ecommerce.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springproject.ecommerce.model.ShoppingCart;
-
-public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
-}
-
