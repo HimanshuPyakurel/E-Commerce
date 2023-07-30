ef1cbc8a9d8cd71fd0c1500cc1ba9f7fa504f840
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-07-30 22:09:54 +0545

    mdoel: Address

--- E-Commerce/src/main/java/com/springproject/ecommerce/model/Address.java
+++ E-Commerce/src/main/java/com/springproject/ecommerce/model/Address.java
@@ -1,6 +1,5 @@
 package com.springproject.ecommerce.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,36 +12,24 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name = "address")
-public class Address {
-
-	
+public class Address {	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id", nullable = false)
 	private int id;
 
-	@Column(name = "address_line_1", nullable = false, length = 512)
-	private String addressLine1;
-
-    @Column(name = "address_line_2", length = 512)
-	private String addressLine2;
-
-	@Column(name = "city", nullable = false)
-	private String city;
-	  
-	@Column(name = "state", nullable = false, length = 75)
-	private String state;
-	
-	@Column(name = "country", nullable = false, length = 75)
+	private String fname;
+	private String lname;
 	private String country;
-	
-	@Column(name = "shipping")
-    private boolean shipping;
-
-    @Column(name = "billing")
-    private boolean billing;
+	private String street;
+	private String city;
+	private String state;
+	private String zip;
+	private String phone;
+	private String notes;
+	private String email;
 	  
-	@ManyToOne(optional = false)
-	@JoinColumn(name = "user_id", nullable = false)
+	@ManyToOne
+	@JoinColumn(name = "user_id")
 	private User user;
+	
 }
