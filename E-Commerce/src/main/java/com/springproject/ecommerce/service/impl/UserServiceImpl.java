03d30246ef5bd5a0f47f8793de09df655e59683d
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-08-01 22:13:30 +0545

    UserServiceImpl

--- E-Commerce/src/main/java/com/springproject/ecommerce/service/impl/UserServiceImpl.java
+++ E-Commerce/src/main/java/com/springproject/ecommerce/service/impl/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.springproject.ecommerce.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -45,6 +47,12 @@ public class UserServiceImpl implements IUserService {
 	public User findByUname(String uname) {
 		
 		return userRepo.findByUname(uname);
+	}
+
+	@Override
+	public List<User> getAllUser() {
+		
+		return userRepo.findAll();
 	}
 
 
