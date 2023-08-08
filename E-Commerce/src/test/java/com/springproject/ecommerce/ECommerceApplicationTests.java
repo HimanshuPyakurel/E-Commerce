6066ab809f924ef233b97ccf04fbf58c355e92f7
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-08-08 21:43:13 +0545

    test

---
+++ E-Commerce/src/test/java/com/springproject/ecommerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springproject.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
