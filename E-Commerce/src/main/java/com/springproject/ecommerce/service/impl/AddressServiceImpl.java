c500f4102dfaedbd3387322bc481fe735ca88316
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-08-10 18:09:47 +0545

    AddressService Implementation class

--- E-Commerce/src/main/java/com/springproject/ecommerce/service/impl/AddressServiceImpl.java
+++ E-Commerce/src/main/java/com/springproject/ecommerce/service/impl/AddressServiceImpl.java
@@ -1,7 +1,5 @@
 package com.springproject.ecommerce.service.impl;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.springproject.ecommerce.model.Address;
@@ -18,12 +16,6 @@ public class AddressServiceImpl implements AddressService {
 	public void addAddress(Address address) {
 		addressRepo.save(address);
 		
-	}
-
-	@Override
-	public List<Address> findAddressByUserId(Long userId) {
-		
-		return addressRepo.findAddressByUserId(userId);
 	}
 
 
