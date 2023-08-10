9d570352804c4827045bdf9928d8a248d8dd12b7
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-08-10 18:09:24 +0545

    service: AddressService

--- E-Commerce/src/main/java/com/springproject/ecommerce/service/AddressService.java
+++ E-Commerce/src/main/java/com/springproject/ecommerce/service/AddressService.java
@@ -1,12 +1,9 @@
 package com.springproject.ecommerce.service;
 
-import java.util.List;
-
 import com.springproject.ecommerce.model.Address;
 
 public interface AddressService {
 
 	void addAddress(Address address);
-	
-	List<Address> findAddressByUserId(Long userId);
+
 }
