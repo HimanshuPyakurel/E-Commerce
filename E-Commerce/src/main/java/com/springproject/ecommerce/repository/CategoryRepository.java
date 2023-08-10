f3310511f892c29b9e8f56ac5825341d047472af
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-08-10 18:09:06 +0545

    repository: Category

--- E-Commerce/src/main/java/com/springproject/ecommerce/repository/CategoryRepository.java
+++ E-Commerce/src/main/java/com/springproject/ecommerce/repository/CategoryRepository.java
@@ -1,9 +1,7 @@
 package com.springproject.ecommerce.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.springproject.ecommerce.model.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
-
 }
