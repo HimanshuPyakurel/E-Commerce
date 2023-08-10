67712cb1244be8598098a17807f84b2837ec80ea
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-08-10 18:08:53 +0545

    repository: Address

--- E-Commerce/src/main/java/com/springproject/ecommerce/repository/AddressRepository.java
+++ E-Commerce/src/main/java/com/springproject/ecommerce/repository/AddressRepository.java
@@ -1,11 +1,8 @@
 package com.springproject.ecommerce.repository;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.springproject.ecommerce.model.Address;
 
 public interface AddressRepository  extends JpaRepository<Address, Integer>{
 
-	List<Address> findAddressByUserId(Long userId);
 }
