d35b0aa5fe407bcd87a6f3c43444a709ff34a89a
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-08-11 11:14:52 +0545

    controller: ShopController

--- E-Commerce/src/main/java/com/springproject/ecommerce/controller/ShopController.java
+++ E-Commerce/src/main/java/com/springproject/ecommerce/controller/ShopController.java
@@ -1,11 +1,16 @@
 package com.springproject.ecommerce.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.springproject.ecommerce.model.Product;
 import com.springproject.ecommerce.service.CategoryService;
 import com.springproject.ecommerce.service.ProductService;
 
@@ -25,5 +30,19 @@ public class ShopController {
 		model.addAttribute("catlist",categoryservice.getAllCategory());
 		return "shop";
 	}
+	
+	 @GetMapping("/find")
+	 public String findProductById(@RequestParam Long id, Model model){
+		 		
+		 List<Product> products = prodService.findProductByCategoryId(id);
+	     
+	     model.addAttribute("prodlist", products);
+	     model.addAttribute("catlist",categoryservice.getAllCategory());
+	        
+	     return "shop";
+	     
+	    }
+	 
+	 
 	
 }
